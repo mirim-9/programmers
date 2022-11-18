df08a4f6885016c1d7b5ce88c269854632b81abd
SOWON <48439367+devSOWON0628@users.noreply.github.com>
2022-11-19 01:35:17 +0900

    더 효율적으로

--- level_3/야근_지수/devSOWON0628.js
+++ level_3/야근_지수/devSOWON0628.js
@@ -1,16 +1,19 @@
 function solution(n, works) {
-    if(works.reduce((a,b)=>a+b) < n) return 0
-    works.sort((a,b)=>{return a-b})
-    let index = works.length-1
+    if(works.reduce((sum, num)=>sum+num,0) < n){
+        return 0
+    }
+    works.sort((a, b)=>{return a-b})
     while(n){
-        let standard = works[index]
-        for(let i = index ; i >= 0; i--){
-            if(works[i] >= standard){
+        let standard = works[works.length-1]
+        for(let i=works.length-1 ; i>=0 ; i--){
+            if(standard == works[i]){
                 works[i]--
                 n--
+            }else{
+                break
             }
             if(n<=0)break
-        }        
+        }
     }
-    return works.map((x)=>{return x*x}).reduce((sum, add)=> sum+add)
+    return works.reduce((a,b)=> a + Math.pow(b,2), 0);
 }
